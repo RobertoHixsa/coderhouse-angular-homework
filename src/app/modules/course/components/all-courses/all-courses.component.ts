73c5f9299c276c7d323f1e5a56f4f5fed2463b4b
RoberoHixsa <roberto.camacho@hixsa.com>
2023-11-01 21:12:16 -0600

    Segunda entrega Roberto Camacho

--- src/app/modules/course/components/all-courses/all-courses.component.ts
+++ src/app/modules/course/components/all-courses/all-courses.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import Course from '../../../../models/Course.model';
 import { CourseService } from '../../../../services/course.service';
-import { filter, from } from 'rxjs';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-all-courses',
@@ -9,36 +8,19 @@ import { filter, from } from 'rxjs';
   styleUrls: ['./all-courses.component.scss'],
 })
 export class AllCoursesComponent implements OnInit {
-  courses: Course[] = [
-    {
-      name: 'React',
-      durationInHours: 50,
-    },
-    {
-      name: 'Django',
-      durationInHours: 40,
-    },
-    {
-      name: 'Spring',
-      durationInHours: 60,
-    },
-  ];
-  coursesObservable!: any;
   source: any;
 
-  constructor(private courseService: CourseService) {
-    this.coursesObservable = this.courseService.getCourses();
-    this.source = from(this.courses).pipe(
-      filter((course) => {
-        return course.durationInHours > 40;
-      })
-    );
-  }
+  shortCourses = this.courseService.coursesObservable$.pipe(
+    map((courses) => courses.filter((course) => course.durationInHours < 50))
+  );
+
+  longCourses = this.courseService.coursesObservable$.pipe(
+    map((courses) => courses.filter((course) => course.durationInHours >= 50))
+  );
+
+  constructor(private courseService: CourseService) {}
 
   ngOnInit(): void {
-    this.courseService.getCourses().subscribe({
-      next: (courses) => {},
-    });
     this.courseService.loadCourses();
   }
 }
--- src/app/services/course.service.ts
+++ src/app/services/course.service.ts
@@ -1,44 +1,31 @@
 import { Injectable } from '@angular/core';
 import { Course } from '../models';
-import { BehaviorSubject, Observable, filter, from } from 'rxjs';
+import { BehaviorSubject, shareReplay } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CourseService {
   private courses$ = new BehaviorSubject<Course[]>([]);
-  private coursesObservable$ = this.courses$.asObservable();
 
-  courses: Course[] = [
-    {
-      name: 'React',
-      durationInHours: 50,
-    },
-    {
-      name: 'Django',
-      durationInHours: 40,
-    },
-    {
-      name: 'Spring',
-      durationInHours: 60,
-    },
-  ];
-
-  private source = from(this.courses).pipe(
-    filter((course) => {
-      return course.durationInHours > 40;
-    })
-  );
-
-  addCourse(course: Course) {
-    this.courses.push(course);
-  }
+  coursesObservable$ = this.courses$.asObservable().pipe(shareReplay());
 
   loadCourses() {
-    this.courses$.next(this.courses);
-  }
-
-  getCourses(): Observable<Course> {
-    return this.source;
+    this.courses$.next(courses);
   }
 }
+
+const courses = [
+  {
+    name: 'React',
+    durationInHours: 50,
+  },
+  {
+    name: 'Django',
+    durationInHours: 40,
+  },
+  {
+    name: 'Spring',
+    durationInHours: 60,
+  },
+];
